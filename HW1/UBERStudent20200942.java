8f371de3c9795e2145600a856be97809bc485fd7
hyeyoung <hyeyoung>
2023-05-12 22:37:08 +0900

    1037

--- HW1/UBERStudent20200942.java
+++ HW1/UBERStudent20200942.java
@@ -2,8 +2,6 @@ import java.io.IOException;
 import java.util.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-
-
 import org.apache.hadoop.conf.*;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
@@ -13,49 +11,56 @@ import org.apache.hadoop.mapreduce.lib.input.*;
 import org.apache.hadoop.mapreduce.lib.output.*;
 import org.apache.hadoop.util.GenericOptionsParser;
 
-public class UBERStudent20200942 {
+public class UBERStudent20200942 
+{
+	public static class WordCountMapper extends Mapper<Object, Text, Text, IntWritable> {
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+		private Text value = new Text();
 
-	public static class Map extends Mapper<Text, Text, Text, Text> {
-	private Text name = new Text();
-	private Text value = new Text();
-	
-	public void map(Text key, Text value, Context context) throws IOException, InterruptedException {
-		//분류별로 나누기?
-		//ex B02512,1/1/2018,190,1132
-		String arr[] = {"MON", "TUE", "WED", "THR", "FRI", "SAT", "SUN"};
-		
-		StringTokenizer itr = new StringTokenizer(value.toString(), ",");
-		
-		String baseNumber = itr.nextToken().trim();
-		String date = itr.nextToken().trim();
-		String activeVehicles = itr.nextToken().trim();
-		String trips = itr.nextToken().trim();
-		
-		try {
-			SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");		
-			Date dateObj = dateFormat.parse(date);
-		
-			Calendar cal = Calendar.getInstance() ;
-			cal.setTime(dateObj);
-				     	
-			int day = cal.get(Calendar.DAY_OF_WEEK) ;
-			String dayOfWeek = arr[day];
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException 
+		{
+			//분류별로 나누기?
+			//ex B02512,1/1/2018,190,1132
+			String arr[] = {"MON", "TUE", "WED", "THR", "FRI", "SAT", "SUN"};
+			
+			StringTokenizer itr = new StringTokenizer(value.toString(), ",");
+			
+			String baseNumber = itr.nextToken().trim();
+			String date = itr.nextToken().trim();
+			int activeVehicles = Integer.parseInt(itr.nextToken().trim());
+			int trips = Integer.parseInt(itr.nextToken().trim());
 			
 			
-			name.set(baseNumber + "," + dayOfWeek);
-			value.set(activeVehicles + "," + trips);	
-	     	}
-	     	catch(Exception e) { }
+			try {
+				SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");		
+				Date dateObj = dateFormat.parse(date);
+			
+				Calendar cal = Calendar.getInstance() ;
+				cal.setTime(dateObj);
+					     	
+				int day = cal.get(Calendar.DAY_OF_WEEK) ;
+				String dayOfWeek = arr[day];
+				
+				
+				word.set(baseNumber + "," + dayOfWeek);
+				value.set(one);	
+		     	}
+		     	catch(Exception e) { 
+		     		
+		     	}
 
 
-		context.write(name, value);
-		}
+			context.write(word, value);
+		}	
 	}
-	
-	public static class Reduce extends Reducer<Text, Text, Text, Text> {
-	private Text result = new Text();
-	
-		public void reduce(Text key, Iterable<Text> value, Context context) throws IOException, InterruptedException {
+
+	public static class WordCountReducer extends Reducer<Text,Text,Text,Text> 
+	{
+		private IntWritable result = new IntWritable();
+
+		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException 
+		{
 			//같은 차량 같은 요일끼리 묶기
 			
 			int vehiclesSum = 0;
@@ -69,32 +74,30 @@ public class UBERStudent20200942 {
 			}
 			
 			result.set(vehiclesSum + "," + tripsSum);
-			context.write(key, result);			
+			context.write(key, result);	
 		}
 	}
-	
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception 
+	{
 		Configuration conf = new Configuration();
 		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
-		if (otherArgs.length != 2)
+		if (otherArgs.length != 2) 
 		{
-			System.err.println("Usage: IMDB <in> <out>");
+			System.err.println("Usage: wordcount <in> <out>");
 			System.exit(2);
 		}
-		    	
 		Job job = new Job(conf, "UBERStudent20200942");
 		job.setJarByClass(UBERStudent20200942.class);
-		job.setMapperClass(Map.class);
-		job.setReducerClass(Reduce.class);
-		job.setCombinerClass(Reduce.class);
+		job.setMapperClass(WordCountMapper.class);
+		job.setCombinerClass(WordCountReducer.class);
+		job.setReducerClass(WordCountReducer.class);
 		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(LongWritable.class);
-		job.setInputFormatClass(TextInputFormat.class);
-		job.setOutputFormatClass(TextOutputFormat.class);
+		job.setOutputValueClass(IntWritable.class);
 		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
-		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));	
-		job.waitForCompletion(true);
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+		FileSystem.get(job.getConfiguration()).delete( new Path(otherArgs[1]), true);
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 }
 
