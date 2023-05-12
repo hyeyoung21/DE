5c2a24087f566aebca4b09e035531e54896b6658
hyeyoung <hyeyoung>
2023-05-12 11:33:32 +0900

    20230512-1133

--- HW1/IMDBStudent20200942.java
+++ HW1/IMDBStudent20200942.java
@@ -10,74 +10,77 @@ import org.apache.hadoop.mapreduce.lib.input.*;
 import org.apache.hadoop.mapreduce.lib.output.*;
 import org.apache.hadoop.util.GenericOptionsParser;
 
-public class IMDBStudent20200942 {
+public class IMDBStudent20200942 
+{
 
-	public static class Map extends Mapper<LongWritable, Text, Text, LongWritable> {
-	private final LongWritable one = new LongWritable(1);
-	private Text genre = new Text();
-		
-		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+	public static class WordCountMapper extends Mapper<Object, Text, Text, IntWritable>
+	{
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException 
+		{
 			//장르별로 나누기
 			//ex 1::Toy Story (1995)::Animation|Children's|Comedy
 			
 			StringTokenizer itr = new StringTokenizer(value.toString(), "::");
 			
-			String[] arr = new String[4]; // 결과 배열
+			String[] arr = new String[4]; 
 			int idx = 0;
 			while (itr.hasMoreTokens()){
 			    arr[idx] = itr.nextToken(); // 배열에 한 토큰씩 담기
 			    idx++;
 			}
 			
-			itr = new StringTokenizer(arr[2], "|");
+			if (arr[arr.length-1] != null)
+				itr = new StringTokenizer(arr[arr.length-1], "|");
+			else
+				itr = new StringTokenizer(arr[arr.length-2], "|");
 			
-			
-			while (itr.hasMoreTokens()) 
+			while (itr.hasMoreTokens())
 			{
-				genre.set(itr.nextToken());
-				context.write(genre, one);
-			}
-		
+				word.set(itr.nextToken());
+				context.write(word, one);
+			}		
+
 		}
 	}
-	
-	public static class Reduce extends Reducer<Text, LongWritable, Text, LongWritable> {
-	private LongWritable result = new LongWritable();
-	
-		public void reduce(Text key, Iterable<LongWritable> value, Context context) throws IOException, InterruptedException {
-			//카운트 하기
+
+	public static class WordCountReducer extends Reducer<Text,IntWritable,Text,IntWritable> 
+	{
+		private IntWritable result = new IntWritable();
+
+		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException 
+		{
 			int sum = 0;
-			
-			for (LongWritable val : value) 
+			for (IntWritable val : values) 
 			{
 				sum += val.get();
 			}
 			result.set(sum);
-			context.write(key, result);			
+			context.write(key, result);
 		}
 	}
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception 
+	{
 		Configuration conf = new Configuration();
 		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
-		if (otherArgs.length != 2)
+		if (otherArgs.length != 2) 
 		{
-			System.err.println("Usage: IMDB <in> <out>");
+			System.err.println("Usage: wordcount <in> <out>");
 			System.exit(2);
 		}
-		    	
 		Job job = new Job(conf, "IMDBStudent20200942");
 		job.setJarByClass(IMDBStudent20200942.class);
-		job.setMapperClass(Map.class);
-		job.setReducerClass(Reduce.class);
-		job.setCombinerClass(Reduce.class);
+		job.setMapperClass(WordCountMapper.class);
+		job.setCombinerClass(WordCountReducer.class);
+		job.setReducerClass(WordCountReducer.class);
 		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(LongWritable.class);
-		job.setInputFormatClass(TextInputFormat.class);
-		job.setOutputFormatClass(TextOutputFormat.class);
+		job.setOutputValueClass(IntWritable.class);
 		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
 		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
-		job.waitForCompletion(true);
+		FileSystem.get(job.getConfiguration()).delete( new Path(otherArgs[1]), true);
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 }
-
